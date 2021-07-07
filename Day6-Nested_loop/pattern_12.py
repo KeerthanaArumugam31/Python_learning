8fe6a20a7118bf91626b0ee4ead7999d0fafd6bc
KeerthanaArumugam31 <86226885+KeerthanaArumugam31@users.noreply.github.com>
2021-07-07 13:55:29 +0530

    Day6-Nested_loop

--- Day6-Nested_loop/pattern_12.py
+++ Day6-Nested_loop/pattern_12.py
@@ -1,6 +1,9 @@
 for i in range(1,6):
     k=i
-    for i in range(1,6):
-        print(k,end=" ")
+    for j in range(1,6):
+        if k<10:
+            print(" "+str(k),end=" ")
+        else:
+            print(k,end=" ")
         k=k+5
     print()
